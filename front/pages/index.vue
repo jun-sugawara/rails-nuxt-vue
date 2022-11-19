e52dada44e64b95166d8b0477a77908bed68ae00
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2022-11-19 09:52:39 +0900

    ナビゲーションガードの実装

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -37,6 +37,19 @@
        });
       },
     },
+    fetch({
+      store,
+      redirect
+    }) {
+      store.watch(
+        state => state.auth.currentUser,
+        (newUser, oldUser) => {
+          if (!newUser) {
+            return redirect("/login");
+          }
+        }
+      );
+    },
   };
 
 </script>
